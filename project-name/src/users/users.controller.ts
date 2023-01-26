cae8a5470fa6bc4e418fcaf94372c46636ce3d34
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-01-26 13:33:34 +0100

    controller update

--- project-name/src/users/users.controller.ts
+++ project-name/src/users/users.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Logger } from '@nestjs/common'
 import { ApiOperation, ApiResponse } from '@nestjs/swagger'
+import { AlbumDto } from './types/album.dto'
+import { CommentDto } from './types/comment.dto'
+import { PhotoDto } from './types/photo.dto'
+import { PostDto } from './types/post.dto'
+import { TodoDto } from './types/todo.dto'
 import { UserDto } from './types/user.dto'
 import { UsersService } from './users.service'
 
-@Controller('users')
+@Controller('api')
 export class UsersController {
     private readonly logger: Logger
 
@@ -11,7 +16,7 @@ export class UsersController {
         this.logger = new Logger(this.constructor.name)
     }
 
-    @Get('')
+    @Get('users')
     @ApiOperation({
         summary: 'Get all the users',
         description: 'Returns a list of users',
@@ -21,7 +26,77 @@ export class UsersController {
         description: 'All is correct',
         type: [UserDto],
     })
-    async list(): Promise<UserDto[]> {
-        return this.usersService.list()
+    async listUsers(): Promise<UserDto[]> {
+        return this.usersService.listUsers()
+    }
+
+    @Get('posts')
+    @ApiOperation({
+        summary: 'Get all the posts',
+        description: 'Returns a list of posts',
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'All is correct',
+        type: [PostDto],
+    })
+    async listPosts(): Promise<PostDto[]> {
+        return this.usersService.listPosts()
+    }
+
+    @Get('comments')
+    @ApiOperation({
+        summary: 'Get all the comments',
+        description: 'Returns a list of comments',
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'All is correct',
+        type: [CommentDto],
+    })
+    async listComments(): Promise<CommentDto[]> {
+        return this.usersService.listComments()
+    }
+
+    @Get('albums')
+    @ApiOperation({
+        summary: 'Get all the albums',
+        description: 'Returns a list of albums',
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'All is correct',
+        type: [AlbumDto],
+    })
+    async listAlbums(): Promise<AlbumDto[]> {
+        return this.usersService.listAlbums()
+    }
+
+    @Get('photos')
+    @ApiOperation({
+        summary: 'Get all the photos',
+        description: 'Returns a list of photos',
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'All is correct',
+        type: [PhotoDto],
+    })
+    async listPhotos(): Promise<PhotoDto[]> {
+        return this.usersService.listPhotos()
+    }
+
+    @Get('todos')
+    @ApiOperation({
+        summary: 'Get all the todos',
+        description: 'Returns a list of todos',
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'All is correct',
+        type: [TodoDto],
+    })
+    async listTodoos(): Promise<TodoDto[]> {
+        return this.usersService.listTodos()
     }
 }
