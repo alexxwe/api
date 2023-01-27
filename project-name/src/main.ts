2ff9c5b90f2cf80a2b650a96285b80b509904457
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-01-27 12:16:54 +0100

    validation pipe

--- project-name/src/main.ts
+++ project-name/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common'
+import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
@@ -11,6 +11,13 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
     app.enableCors({})
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    )
 
     // Create the swagger documentation
     const swaggerConfig = new DocumentBuilder()
